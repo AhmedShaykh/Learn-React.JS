db9cb61f3fce3335ee8cca280e258137c298255e
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-02-05 15:21:50 +0500

    It's Time To Push !

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hello World
-      </h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <h3>
+          Welcome To Blockchain World.<br/>We Explore Metaverse With Web 3.0 , De-Fi & NFT's Connect In Cryptocurrencies.
+        </h3>
+        <a
+          className="App-link"
+          href="https://coinmarketcap.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <b>
+            We Love Bitcoin
+          </b>
+        </a> */}
+      </header>
     </div>
   );
 }
