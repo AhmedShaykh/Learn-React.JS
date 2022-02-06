1f12c3cd869c82b56686de16464bdfbc1bf74a8f
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-02-06 22:03:11 +0500

    It's Time To Push !

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import Props from './Props';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // const name = 'Ahmed Shaykh';
-  // const name = 'Learning React.JS !';
-  // return (
-  //     // <h1>
-  //     //   Learning React.JS !
-  //     // </h1>
-  //     name
-  //     // <Message />
-  // );
   return (
-    <div>
-      <Props techName='React.JS !' yearName='2020'/>
-      <Props techName='Blockchain !' yearName='2021'/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
     </div>
   );
 }
