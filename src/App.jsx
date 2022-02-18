d2f1812be56ad0547193e3120ca00545f99c92e1
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-02-18 22:33:34 +0500

    It's Time To Push !

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <h1 className='heading'>Good {isMorning ? 'Morning' : 'Night'}</h1>
       <button className='btn' onClick={() => setMorning(!isMorning)}>Update Day</button>
       <Props techName='React.JS !' yearName='2020' />
-      <Props techName='Blockchain !' yearName='2021' />
+      <Props techName='Blockchain !' yearName='2021 To 2023' />
       <UseState />
     </div>
   );
